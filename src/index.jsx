0c22a2a71a9eedc5dad87debea2e2a46e18f0c98
Araytar <araytar@gmail.com>
2025-03-31 16:14:14 +0200

    added index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+export const ShipMonitor = () => {
+  return (
+    <div>
+      <h1>ANNIK-A Car-Go Monitor</h1>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ShipMonitor />);
